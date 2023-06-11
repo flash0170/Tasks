8382fc4a0ddf73205a1941218c0cbc5a79681061
Boris <Boris@gmail.com>
2023-06-11 22:03:47 +0300

    Задача №1

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №1
+
+Console.WriteLine("Введите число а ");
+int a = int.Parse(Console.ReadLine()!); 
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine()!); 
+if (a == b*b)
+{
+ Console.WriteLine("Число а являеться квадратом b");   
+}
+else
+{
+ Console.WriteLine("Число а НЕ являеться квадратом b");   
+}
