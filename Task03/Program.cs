4717dec48bf07e82dee85caa60051114299cc4d3
Boris <Boris@gmail.com>
2023-06-11 22:40:42 +0300

    Задача №3

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №3
+Console.WriteLine("Введите номер дня нелели:");
+int number = int.Parse(Console.ReadLine()!); 
+
+if (number == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    else if (number == 5 )
+    {
+        Console.WriteLine("Пятница");
+    }
+    else
+        {
+            Console.WriteLine("Неопределено");
+        }
