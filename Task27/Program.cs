e71318c92a98be260519054d102814b5115db9d1
Boris <Boris@gmail.com>
2023-06-27 15:54:39 +0300

    Выдаёт сумму цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int ReadDataStr(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+int Summa(int num)
+{
+    int res = 0;
+    while (num >0)
+    {
+        res = res + num%10;
+        num = num /10;
+    }
+    return res;
+}
+
+int number = ReadDataStr("Введите положительное, целое число: ");
+
+DateTime d2 = DateTime.Now;
+int summa = Summa(number);
+
+PrintData("Сумма цифр числа равна: ", summa);
