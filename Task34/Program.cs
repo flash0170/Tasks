54d4bfa3d18318160ac3fc341d200b8dd50a38d6
Boris <Boris@gmail.com>
2023-06-30 13:20:35 +0300

    Показывает количество чётных чисел в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int CountElem(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (Test(arr[i]))
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+bool Test(int n)
+{
+    return (n % 2 == 0);
+}
+
+
+int[] Array1 = GenArray(10,100,999);
+
+
+PrintArray(Array1);
+
+int count = CountElem(Array1);
+
+PrintData("Количество чётных чисел в массиве: "+count);
