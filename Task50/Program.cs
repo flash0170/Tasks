b6d7363385d36359b8ef7aacbdc6144e64eeccab
flash0170 <135309211+flash0170@users.noreply.github.com>
2023-07-02 18:32:00 +0300

    Находит элемент в двумерном массиве

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -79,12 +79,12 @@ Console.WriteLine("\n=== ДВУМЕРНЫЙ МАССИВ ===\n");
 // Получение данных от пользователя
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("\n=== ВВЕДИТЕ ДАННЫЕ МАССИВА ===");
-int column = ReadData("Количество столбцов массива: ");
-int row = ReadData("Количество  строк   массива: ");
+int column = ReadData("Количество  строк   массива: ");
+int row = ReadData("Количество столбцов массива: ");
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("\n=== ВВЕДИТЕ ДАННЫЕ ИСКОМОГО ЭЛЕМЕНТА ===");
-int findeVer = ReadData("Введите позицию по   вертикали: ");
 int findeHor = ReadData("Введите позицию по горизонтали: ");
+int findeVer = ReadData("Введите позицию по   вертикали: ");
 
 
 // Генерируем двумерный массив
