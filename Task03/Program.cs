9ba5dacc73d501c5f008cf2f68a36d3450a777d1
Boris <Boris@gmail.com>
2023-07-05 19:44:28 +0300

    По введенной цифре выводит день недели

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,16 +1,34 @@
-﻿// Задача №3
-Console.WriteLine("Введите номер дня нелели:");
-int number = int.Parse(Console.ReadLine()!); 
+﻿﻿// Задача №3
+// Напишите программу, которая на вход принимает
+// число сообшает день недели
 
-if (number == 3)
+// Вводим данные с консоли первое число
+Console.WriteLine("Введите число от 1 до 7");
+string? inputNum = Console.ReadLine();
+
+// if проверка на нулевое значение
+if (inputNum != null)
+{
+    //конвертация строки в число
+    int nameDay = int.Parse(inputNum);
+       
+    // объявляем ПУСТУЮ переменную
+    string outDayOfWeek = string.Empty;
+
+    // Перебираем оператором Switch и присваиваем значение
+    switch (nameDay)
     {
-        Console.WriteLine("Среда");
+        case 1: outDayOfWeek = "Понедельник"; break;
+        case 2: outDayOfWeek = "Вторник"; break;
+        case 3: outDayOfWeek = "Среда"; break;
+        case 4: outDayOfWeek = "Четверг"; break;
+        case 5: outDayOfWeek = "Пятница"; break;
+        case 6: outDayOfWeek = "Суббота"; break;
+        case 7: outDayOfWeek = "Воскресенье"; break;
+        default: outDayOfWeek = "Это не день недели, повторите ввод"; break;
     }
-    else if (number == 5 )
-    {
-        Console.WriteLine("Пятница");
-    }
-    else
-        {
-            Console.WriteLine("Неопределено");
-        }
+
+    // Вывод результата
+    Console.WriteLine(outDayOfWeek);
+
+}
