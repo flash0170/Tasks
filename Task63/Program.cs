cedf088bdbe6a04645aebbe7507957cc7675c45e
Boris <Boris@gmail.com>
2023-07-07 17:25:35 +0300

    Выводит все натуральные числа в промежутке от 1 до N

---
+++ Task63/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите чило N -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вывод результата -> ");
+Console.WriteLine($"{Range(n)}");
+
+string Range (int n)
+{
+    if(n == 1)
+    {
+        return "1";
+    }
+    return Range(n-1) + ", " + n;
+}
