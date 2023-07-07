9807b4c53dbb70d4c5131b86ad03dd05e771c022
Boris <Boris@gmail.com>
2023-07-07 17:29:11 +0300

    Показывает сумму цифр введенного числа

---
+++ Task67/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 67: Напишите программу, которая
+// будет принимать на вход число и
+// возвращать сумму его цифр.
+
+// 453 -> 12
+// 45 -> 9
+
+int number = Prompt("Введите число ->  ");
+Console.Write("Сумма цифр -> ");
+Console.WriteLine($"{SumDigit(number)}");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int SumDigit(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return SumDigit(n / 10) + (n % 10);
+}
